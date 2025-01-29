af396f95caffff36f6d1984841bd16278be39222
kopaka <felixbruell@live.de>
2025-01-29 13:01:54 +0100

    changed dlss to be evaluated last

--- scripts/DitherV1.py
+++ scripts/DitherV1.py
@@ -1,18 +1,19 @@
 from pathlib import WindowsPath, PosixPath
 from falcor import *
 
-def render_graph_DefaultRenderGraph():
+def render_graph_Dither():
     g = RenderGraph('Dither')
     g.create_pass('VBufferLighting', 'VBufferLighting', {'envMapIntensity': 0.25, 'ambientIntensity': 0.25, 'lightIntensity': 0.5, 'envMapMirror': False})
     g.create_pass('DitherVBuffer', 'DitherVBuffer', {})
-    g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
+    g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Linear', 'clamp': False, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
     g.create_pass('TAA', 'TAA', {'alpha': 0.10000000149011612, 'colorBoxSigma': 1.0, 'antiFlicker': False})
-    g.create_pass('DLSSPass', 'DLSSPass', {'enabled': True, 'outputSize': 'Default', 'profile': 'DLAA', 'motionVectorScale': 'Relative', 'isHDR': True, 'useJitteredMV': False, 'sharpness': 0.0, 'exposure': 0.0})
+    g.create_pass('DLSSPass', 'DLSSPass', {'enabled': True, 'outputSize': 'Default', 'profile': 'DLAA', 'motionVectorScale': 'Relative', 'isHDR': True, 'useJitteredMV': False, 'sharpness': 0.3499999940395355, 'exposure': 0.0})
     g.create_pass('UnpackVBuffer', 'UnpackVBuffer', {})
     g.create_pass('RayShadow', 'RayShadow', {})
-    g.create_pass('ImageEquation', 'ImageEquation', {'formula': 'I0[xy]', 'format': 'RGBA32Float'})
+    g.create_pass('ToneMapperTAA', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Linear', 'clamp': False, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
+    g.create_pass('AccumulatePass', 'AccumulatePass', {'enabled': True, 'outputSize': 'Default', 'autoReset': True, 'precisionMode': 'Single', 'maxFrameCount': 0, 'overflowMode': 'Stop'})
+    g.create_pass('ToneMapperAccumulate', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Linear', 'clamp': False, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
     g.add_edge('DitherVBuffer.vbuffer', 'VBufferLighting.vbuffer')
-    g.add_edge('VBufferLighting.color', 'ToneMapper.src')
     g.add_edge('DitherVBuffer.mvec', 'TAA.motionVecs')
     g.add_edge('DitherVBuffer.mvec', 'DLSSPass.mvec')
     g.add_edge('DitherVBuffer.depth', 'DLSSPass.depth')
@@ -20,13 +21,17 @@ def render_graph_DefaultRenderGraph():
     g.add_edge('UnpackVBuffer.posW', 'RayShadow.posW')
     g.add_edge('UnpackVBuffer.normalW', 'RayShadow.normalW')
     g.add_edge('RayShadow.visibility', 'VBufferLighting.visibilityBuffer')
-    g.add_edge('ToneMapper.dst', 'ImageEquation.I0')
-    g.add_edge('ImageEquation.out', 'DLSSPass.color')
-    g.add_edge('ImageEquation.out', 'TAA.colorIn')
-    g.mark_output('DLSSPass.output')
-    g.mark_output('TAA.taaOut')
+    g.add_edge('VBufferLighting.color', 'DLSSPass.color')
+    g.add_edge('VBufferLighting.color', 'TAA.colorIn')
+    g.add_edge('DLSSPass.output', 'ToneMapper.src')
+    g.add_edge('VBufferLighting.color', 'AccumulatePass.input')
+    g.add_edge('AccumulatePass.output', 'ToneMapperAccumulate.src')
+    g.add_edge('TAA.colorOut', 'ToneMapperTAA.src')
+    g.mark_output('ToneMapper.dst')
+    g.mark_output('ToneMapperTAA.dst')
+    g.mark_output('ToneMapperAccumulate.dst')
     return g
 
-DefaultRenderGraph = render_graph_DefaultRenderGraph()
-try: m.addGraph(DefaultRenderGraph)
+Dither = render_graph_Dither()
+try: m.addGraph(Dither)
 except NameError: None
