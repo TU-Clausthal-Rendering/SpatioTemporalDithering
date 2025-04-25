52cecd9d99d6f2d66e26a122dcef38708659dd2a
Kopaka <felixbruell@live.de>
2025-04-25 12:02:58 +0200

    started dynamic fragment

---
+++ Source/RenderPasses/RasterOITDynFragment/RasterOITDynFragment.cpp
@@ -0,0 +1,218 @@
+/***************************************************************************
+ # Copyright (c) 2015-23, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include "RasterOITDynFragment.h"
+
+#include "Scene/Lighting/LightSettings.h"
+#include "Scene/Lighting/ShadowSettings.h"
+
+namespace
+{
+    const std::string kDepth = "depth"; // input depth buffer
+    const std::string kColor = "color";
+
+    const std::string kWhitelist = "whitelist";
+
+    const std::string kProgramFile = "RenderPasses/RasterOITDynFragment/BuildList.3D.slang";
+    const std::string kSortFile = "RenderPasses/RasterOITDynFragment/SortList.slang";
+}
+
+
+extern "C" FALCOR_API_EXPORT void registerPlugin(Falcor::PluginRegistry& registry)
+{
+    registry.registerClass<RenderPass, RasterOITDynFragment>();
+}
+
+RasterOITDynFragment::RasterOITDynFragment(ref<Device> pDevice, const Properties& props)
+    : RenderPass(pDevice)
+{
+    // default graphics state
+    mpState = GraphicsState::create(mpDevice);
+    DepthStencilState::Desc dsDesc;
+    dsDesc.setDepthWriteMask(false); // disable depth writes
+    auto dsState = DepthStencilState::create(dsDesc);
+    mpState->setDepthStencilState(dsState);
+    mpFbo = Fbo::create(mpDevice);
+
+    // sort pass
+    //Program::Desc desc;
+    //desc.addShaderLibrary(kSortFile).csEntry("main").setShaderModel("6_5");
+
+    //mpSortPass = ComputePass::create(mpDevice, desc);
+}
+
+Properties RasterOITDynFragment::getProperties() const
+{
+    return {};
+}
+
+RenderPassReflection RasterOITDynFragment::reflect(const CompileData& compileData)
+{
+    // Define the required resources here
+    RenderPassReflection reflector;
+    reflector.addInput(kDepth, "Depth buffer");
+
+    reflector.addOutput(kColor, "Color buffer").format(ResourceFormat::RGBA16Float).bindFlags(ResourceBindFlags::AllColorViews);
+    return reflector;
+}
+
+void RasterOITDynFragment::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    auto pDepth = renderData.getTexture(kDepth);
+    auto pColor = renderData.getTexture(kColor);
+
+    uint2 dim = uint2(pDepth->getWidth(), pDepth->getHeight());
+    setupCountBuffer(mpCountBuffer, dim);
+    mpCountBuffer->setName("CountBuffer");
+    setupCountBuffer(mpPrefixBuffer, dim);
+    mpPrefixBuffer->setName("PrefixBuffer");
+
+    // clear resources
+    uint32_t uintmax = uint32_t(-1);
+    pRenderContext->clearTexture(pColor.get(), float4(0, 0, 0, 1));
+    pRenderContext->clearUAV(mpCountBuffer->getUAV().get(), uint4(0));
+
+    pRenderContext->uavBarrier(mpCountBuffer.get());
+
+    if (!mpScene)
+    {
+        return;
+    }
+
+    if (!mpDataBuffer || mpDataBuffer->getElementCount() != mDataBufferSize)
+    {
+        mpDataBuffer = Buffer::createStructured(mpDevice, sizeof(uint4), mDataBufferSize, ResourceBindFlags::UnorderedAccess | ResourceBindFlags::ShaderResource, Buffer::CpuAccess::None);
+    }
+
+    // obtain whitelist
+    std::set<std::string> whitelist;
+    const bool useWhitelist = renderData.getDictionary().keyExists(kWhitelist);
+    if (useWhitelist)
+        whitelist = renderData.getDictionary().getValue<decltype(whitelist)>(kWhitelist);
+
+    // setup culling
+    auto camera = mpScene->getCamera();
+    if (!mpCulling)
+    {
+        mpCulling = make_ref<FrustumCulling>(camera);
+    }
+
+    mpCulling->setUserCallback([&](const MeshDesc& mesh)
+        {
+            if (!useWhitelist) return true; // draw all transparent objects
+            auto mat = mpScene->getMaterial(MaterialID::fromSlang(mesh.materialID));
+            auto name = mat->getName();
+            return whitelist.find(name) != whitelist.end();
+        });
+
+    auto cameraChanges = camera->getChanges();
+    auto excluded = Camera::Changes::Jitter | Camera::Changes::History;
+    if (((cameraChanges & ~excluded) != Camera::Changes::None))
+    {
+        mpCulling->updateFrustum(camera);
+    }
+
+    // framebuffer setup (only depth)
+    mpFbo->attachDepthStencilTarget(pDepth);
+    mpState->setFbo(mpFbo);
+
+    // setup vars and program
+    auto vars = mpVars->getRootVar();
+    vars["gCounts"] = mpCountBuffer;
+    vars["gBuffer"] = mpDataBuffer;
+    vars["gPrefix"] = mpPrefixBuffer;
+
+    vars["PerFrame"]["gFrameDim"] = dim;
+    vars["PerFrame"]["maxElements"] = mpDataBuffer->getElementCount();
+
+    // lighting settings
+    LightSettings::get().updateShaderVar(vars);
+    ShadowSettings::get().updateShaderVar(mpDevice, vars);
+    mpProgram->addDefines(ShadowSettings::get().getShaderDefines(*mpScene, renderData.getDefaultTextureDims()));
+
+    // count fragments
+    {
+        FALCOR_PROFILE(pRenderContext, "Count");
+
+        mpProgram->addDefine("COUNT", "1");
+        mpState->setProgram(mpProgram);
+
+        mpScene->rasterizeFrustumCulling(pRenderContext, mpState.get(), mpVars.get(), RasterizerState::CullMode::None, RasterizerState::MeshRenderMode::SkipOpaque, true, mpCulling);
+    }
+
+    // scan counters
+
+    // record fragments
+    {
+        FALCOR_PROFILE(pRenderContext, "Record");
+
+        mpProgram->removeDefine("COUNT");
+        mpState->setProgram(mpProgram);
+
+        //mpScene->rasterizeFrustumCulling(pRenderContext, mpState.get(), mpVars.get(), RasterizerState::CullMode::None, RasterizerState::MeshRenderMode::SkipOpaque, true, mpCulling);
+    }
+
+    // sort fragments
+    
+}
+
+void RasterOITDynFragment::renderUI(Gui::Widgets& widget)
+{
+    widget.var("Buffer Node Count", mDataBufferSize, 1024u, 1024u * 1024u * 1024u);
+    auto sizeInBytes = size_t(mDataBufferSize) * size_t(16);
+    widget.text("Size in MB: " + std::to_string(sizeInBytes / (1024u * 1024u)));
+}
+
+void RasterOITDynFragment::setScene(RenderContext* pRenderContext, const ref<Scene>& pScene)
+{
+    mpScene = pScene;
+    setupProgram();
+    mpCulling = nullptr;
+}
+
+void RasterOITDynFragment::setupCountBuffer(ref<Buffer>& buffer, uint2 res) const
+{
+    if (buffer == nullptr || buffer->getElementCount() != res.x * res.y)
+    {
+        buffer = Buffer::createStructured(mpDevice, sizeof(uint), res.x * res.y, ResourceBindFlags::UnorderedAccess | ResourceBindFlags::ShaderResource, Buffer::CpuAccess::None);
+    }
+}
+
+void RasterOITDynFragment::setupProgram()
+{
+    if (!mpScene) return;
+
+    Program::Desc desc;
+    desc.addShaderModules(mpScene->getShaderModules());
+    desc.addShaderLibrary(kProgramFile).psEntry("psMain").vsEntry("vsMain");
+    desc.addTypeConformances(mpScene->getTypeConformances());
+    desc.setShaderModel("6_5");
+
+    mpProgram = GraphicsProgram::create(mpDevice, desc, mpScene->getSceneDefines());
+    mpState->setProgram(mpProgram);
+    mpVars = GraphicsVars::create(mpDevice, mpProgram->getReflector());
+}
